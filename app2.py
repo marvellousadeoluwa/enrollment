408221feeded483f55bce10fdc78c7c389713bc4
Marvellous Alabi <60781267+marvellousadeoluwa@users.noreply.github.com>
2025-07-05 13:48:53 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -28,24 +28,49 @@ feature_list = [
 
 # UI for top 12 features
 input_data = {
-    'StateOfOrigin': st.selectbox("State of Origin", ['Oyo', 'Lagos', 'Osun', 'Ogun', 'Kogi ']),
-    'CurrentLevel': st.selectbox("Current Level", [100, 200, 300, 400, 'Graduate']),
-    'EnrollmentYear': st.selectbox("Enrollment Year", [2019, 2021, 2022, 2023, 2024]),
-    'Department': st.text_input("Department", "Biochemistry"),
     'ReasonForChoosingUniv': st.text_input("Reason for Choosing University", "Quality education"),
-    'TuitionFundingSource': st.selectbox("Tuition Funding Source", ['Parents', 'Scholarship', 'Self', 'Loan', 'Other']),
+    
+    'Faculty': st.selectbox("Faculty", [
+        'COLLEGE OF BASIC MEDICAL AND HEALTH SCIENCES',
+        'College of Natural and applied sciences',
+        'Law',
+        'Natural and Applied Sciences'
+    ]),
+    
+    'Department': st.text_input("Department", "Computer science"),
+    
+    'EnrollmentYear': st.selectbox("Enrollment Year", [2019, 2020, 2021, 2022, 2023, 2024]),
+    
+    'AgeGroup': st.selectbox("Age Group", ['<18', '18-22', '23-27', '28-32', '33+']),
+    
+    'CurrentLevel': st.selectbox("Current Level", [100, 200, 300, 400, 'Graduate']),
+    
     'TransferReason': st.text_input("Reason for Previous Transfer (if any)", "None"),
-    'CGPARange': st.selectbox("CGPA Range", ['2.4 - 3.49', '3.5 - 4.49', '4.5 - 5.0', 'Unknown']),
+    
+    'CGPARange': st.selectbox("CGPA Range", ['2.4 - 3.49', '3.5 - 4.49', '4.5 - 5.0']),
+    
+    'StateOfOrigin': st.selectbox("State of Origin", ['Oyo', 'Lagos', 'Osun', 'Ogun', 'Kogi']),
+    
+    'TuitionFundingSource': st.selectbox("Tuition Funding Source", ['Parents', 'Scholarship', 'Self', 'Loan', 'Other']),
+    
     'StudentSupportRating': st.selectbox("Student Support Rating", ['Excellent', 'Good', 'Fair', 'Poor']),
+    
     'AcademicChallenges': st.selectbox("Academic Challenges", [
-        'None', 'Poor Facilities', 'Financial', 'Lecturer Availability', 'Unknown'
+        'None', 'Poor Facilities', 'Financial', 'Lecturer Availability', 'Time management', 'Religion Palava'
     ])
 }
 
 
+# Age mapping
+age_mapping = {'<18': 16, '18-22': 20, '23-27': 25, '28-32': 30, '33+': 33}
+
+# Derived features
+input_data['EnrollmentAge'] = (2025 - input_data['EnrollmentYear']) + age_mapping[input_data['AgeGroup']]
+input_data['FacultyDepartment'] = input_data['Faculty'] + "_" + input_data['Department']
+input_data['HadAcademicChallenges'] = 0 if input_data['AcademicChallenges'] == 'None' else 1
 
 # Construct DataFrame
-input_df = pd.DataFrame([input_data])
+input_df = pd.DataFrame([input_data[feature_list]])
 
 # Encode categorical values
 for col in input_df.columns:
